eaca270261b64aaefd481ab47bdaefa47d15a079
Kenneth Lin <klin300@yahoo.com>
2013-04-28 18:34:57 -0400

    Revert "GameLogic default constructor"
    This reverts commit 7e24f1449a9e23514357f69e39e48a8e34bcc3c4.

---
+++ src/edu/brown/cs32/jcalder/GameLogic/RogueMap/LogicMap.java
@@ -0,0 +1,67 @@
+package edu.brown.cs32.jcalder.GameLogic.RogueMap;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import java.awt.Point;
+import java.awt.Rectangle;
+
+import edu.brown.cs32.goingrogue.map.RogueMap;
+import edu.brown.cs32.goingrogue.map.Space;
+
+/**
+ *
+ * @author john
+ */
+public class LogicMap implements RogueMap
+{
+    private List<Room> rooms;
+    
+    public LogicMap(List<Room> r)
+    {
+        rooms = r;
+    }
+    
+    public static LogicMap getRandomMap() throws IOException,IllegalArgumentException
+    {
+        File folder = new File("./Maps");
+        File map;
+        List<File> maps = Arrays.asList(folder.listFiles());
+        if(maps.size()==1)
+            map=maps.get(0);
+        else
+            map=maps.get((new Random()).nextInt(maps.size()));
+        return MapReader.readMap(map);
+    }
+    
+    public boolean isValid(Point p)
+    {
+        for(Room r : rooms)
+        {
+            if(r.isValid(p))
+                return true;
+        }
+        return false;
+    }
+    
+    public List<Space> getData(int minX, int minY,int maxX, int maxY)
+    {
+        Rectangle view = new Rectangle(minX,minY,maxX-minX,maxY-minY);
+        List<Space> ret = new ArrayList<>();
+        for(Room r : rooms)
+        {
+            Rectangle rm = new Rectangle(r.getX(),r.getY(),r.getWidth(),r.getHeight());
+            if(view.intersects(rm))
+                ret.add(r);
+            for(Corridor c : r.getCorridors())
+            {
+                if(view.intersects(c.getRectangle()))
+                    ret.add(c);
+            }
+        }
+        return ret;
+    }
+}
---
+++ src/edu/brown/cs32/jcalder/GameLogic/RogueMap/Room.java
@@ -0,0 +1,108 @@
+package edu.brown.cs32.jcalder.GameLogic.RogueMap;
+
+import java.awt.geom.Rectangle2D;
+import java.awt.Point;
+import java.util.List;
+import java.util.ArrayList;
+
+import edu.brown.cs32.goingrogue.map.Space;
+import edu.brown.cs32.goingrogue.map.Tile;
+import edu.brown.cs32.goingrogue.map.Wall;
+
+/**
+ *
+ * @author john
+ */
+public class Room implements Space
+{
+    private Rectangle2D.Double room;
+    private String id;
+    private List<Corridor> connections;
+    
+    public Room(Point p, int w, int h, String id)
+    {
+        room=new Rectangle2D.Double(p.getX(),p.getY(),w,h);
+        connections = new ArrayList<>();
+    }
+    
+    public void addCorridor(Corridor c)
+    {
+        connections.add(c);
+    }
+    
+    public String getID()
+    {
+        return id;
+    }
+    
+    public int getWidth()
+    {
+        return (int)room.getWidth();
+    }
+    
+    public int getHeight()
+    {
+        return (int)room.getHeight();
+    }
+    
+    public int width()
+    {
+        return getWidth();
+    }
+    
+    public int height()
+    {
+        return getHeight();
+    }
+    
+    public Point upperLeft()
+    {
+        return new Point((int)room.getX(),(int)room.getY());
+    }
+    
+    public Tile[][] getFloor()
+    {
+        int x = (int)room.getX();
+        int y = (int)room.getY();
+        Tile[][] ret = new Tile[x][y];
+        for(int i=0;i<x;i++)
+        {
+            for(int j=0;j<y;j++)
+                ret[i][j]=Tile.GROUND;
+        }
+        return ret;
+    }
+    
+    public Wall getWallType()
+    {
+        return Wall.DEFAULT;
+    }
+    
+    public int getX()
+    {
+        return (int)room.getX();
+    }
+    
+    public int getY()
+    {
+        return (int)room.getY();
+    }
+    
+    public boolean isValid(Point p)
+    {
+        if(p.getX()>=room.getX() && p.getX()<=room.getX()+room.getWidth()&&
+           p.getY()>=room.getY() && p.getY()<=room.getY()+room.getHeight())
+            return true;
+        for(Corridor c : connections)
+        {
+            if(c.isValid(p))
+                return true;
+        }
+        return false;
+    }
+    
+    public List<Corridor> getCorridors()
+    {
+        return connections;
+    }
+}
