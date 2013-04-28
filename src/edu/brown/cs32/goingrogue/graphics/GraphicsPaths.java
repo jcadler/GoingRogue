9a989c6e8732121e5c8e44be6a12b2b48add0623
Nico Adams <nicoadams000@gmail.com>
2013-04-28 16:45:43 -0400

    Backup

--- src/edu/brown/cs32/goingrogue/graphics/GraphicsPaths.java
+++ src/edu/brown/cs32/goingrogue/graphics/GraphicsPaths.java
@@ -14,55 +14,55 @@ package edu.brown.cs32.goingrogue.graphics;
  */
 public enum GraphicsPaths {
 
-    GRAPHICS("graphics/"),
-    //A 20x20 empty image
-    EMPTY("empty.png", GRAPHICS),
-    SCENERY("scenery/", GRAPHICS),
-    GROUND("ground.png"),
-    WATER("water.png"),
-    WALLS("walls/", SCENERY),
-    WALL_N("N.png", WALLS),
-    WALL_S("S.png", WALLS),
-    WALL_E("E.png", WALLS),
-    WALL_W("W.png", WALLS),
-    WALL_NE("NE.png", WALLS),
-    WALL_NW("NW.png", WALLS),
-    WALL_SE("SE.png", WALLS),
-    WALL_SW("SW.png", WALLS),
-    CREATURES("creatures/", GRAPHICS),
-    CHARACTERS("characters/", CREATURES),
-    CHAR_1("", CHARACTERS),
-    CHAR_2("", CHARACTERS),
-    CHAR_3("", CHARACTERS),
-    CHAR_4("", CHARACTERS),
-    MONSTERS("monsters/", CREATURES),
-    BAT("bat/", MONSTERS),
-    SLIME("slime/", MONSTERS),
-    // TODO Add (and draw) all of these
+	GRAPHICS("graphics/"),
+	
+	//A 20x20 empty image
+	EMPTY("empty.png", GRAPHICS),
+	
+	SCENERY("scenery/", GRAPHICS),
+		GROUND("ground.png", SCENERY),
+		WATER("water.png", SCENERY),
+		WALLS("walls/", SCENERY),
+			WALL_N("N.png", WALLS),
+			WALL_S("S.png", WALLS),
+			WALL_E("E.png", WALLS),
+			WALL_W("W.png", WALLS),
+			WALL_NE("NE.png", WALLS),
+			WALL_NW("NW.png", WALLS),
+			WALL_SE("SE.png", WALLS),
+			WALL_SW("SW.png", WALLS),
+	
+	CREATURES("creatures/", GRAPHICS),
+		CHARACTERS("characters/", CREATURES),
+			CHAR_1("", CHARACTERS),
+			CHAR_2("", CHARACTERS),
+			CHAR_3("", CHARACTERS),
+			CHAR_4("", CHARACTERS),
+		MONSTERS("monsters/", CREATURES),
+			BAT("bat/", MONSTERS),
+			SLIME("slime/", MONSTERS),
+			EMU("", MONSTERS),
+			DRAGON("", MONSTERS),
+			HOB_GOBLIN("", MONSTERS),
+			GIANT_SPRITE("", MONSTERS),
+			SNAKE_SPRITE("", MONSTERS),
+			DOG_SPRITE("", MONSTERS),
 
-    ITEMS("items/", GRAPHICS);
-    
-    // creature and item sprites
-    public static final String PLAYER_SPRITE = "";
-    public static final String EMU_SPRITE = "";
-    public static final String DRAGON_SPRITE = "";
-    public static final String HOB_GOBLIN_SPRITE = "";
-    public static final String GIANT_SPRITE = "";
-    public static final String SNAKE_SPRITE = "";
-    public static final String DOG_SPRITE = "";
-    public static final String SWORD_SPRITE = "";
-    public static final String AXE_SPRITE = "";
-    public static final String WAR_HAMMER_SPRITE = "";
-    public static final String SPEAR_SPRITE = "";
-    public static final String ARMOUR_SPRITE = "";
-    public static final String SHIELD_SPRITE = "";
-    public static final String POTION_SPRITE = "";
-    public final String path;
-    final GraphicsPaths superpath;
+	ITEMS("items/", GRAPHICS),
+		SWORD_SPRITE("", ITEMS),
+		AXE_SPRITE("", ITEMS),
+		WAR_HAMMER_SPRITE("", ITEMS),
+		SPEAR_SPRITE("", ITEMS),
+		ARMOUR_SPRITE("", ITEMS),
+		SHIELD_SPRITE("", ITEMS),
+		POTION_SPRITE("", ITEMS);
+	
+	public final String path;
+	final GraphicsPaths superpath;
 
-    GraphicsPaths(String _path) {
-        superpath = null;
-        path = _path;
+	GraphicsPaths(String _path) {
+		superpath = null;
+		path = _path;
     }
 
     GraphicsPaths(String _path, GraphicsPaths _superpath) {
