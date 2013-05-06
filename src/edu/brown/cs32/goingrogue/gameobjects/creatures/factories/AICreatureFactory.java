24347bd5819d378d57a52867a1c5d63282473dd0
Ben Weedon <Benjamin_Weedon@brown.edu>
2013-05-05 20:10:08 -0400

    create method now takes list of creatures and rooms.

--- src/edu/brown/cs32/goingrogue/gameobjects/creatures/factories/AICreatureFactory.java
+++ src/edu/brown/cs32/goingrogue/gameobjects/creatures/factories/AICreatureFactory.java
@@ -2,11 +2,13 @@ package edu.brown.cs32.goingrogue.gameobjects.creatures.factories;
 
 import edu.brown.cs32.goingrogue.gameobjects.creatures.AICreature;
 import edu.brown.cs32.goingrogue.gameobjects.creatures.Attribute;
+import edu.brown.cs32.goingrogue.gameobjects.creatures.Creature;
 import edu.brown.cs32.goingrogue.gameobjects.creatures.CreatureStats;
 import java.awt.geom.Point2D;
 import java.util.List;
 import static edu.brown.cs32.goingrogue.gameobjects.creatures.util.RandomDataUtil.*;
 import edu.brown.cs32.goingrogue.util.CreatureSize;
+import edu.brown.cs32.jcadler.GameLogic.RogueMap.Room;
 
 /**
  *
@@ -14,7 +16,7 @@ import edu.brown.cs32.goingrogue.util.CreatureSize;
  */
 public class AICreatureFactory {
 
-    public static AICreature create() {
+    public static AICreature create(List<Creature> creatures, List<Room> rooms) {
         double direction = Math.PI / 2.0;
         List<Attribute> attributes = randomCreatureAttributes();
         String name = getCreatureName(attributes);
--- src/edu/brown/cs32/goingrogue/gameobjects/items/factories/GridItemFactory.java
+++ src/edu/brown/cs32/goingrogue/gameobjects/items/factories/GridItemFactory.java
@@ -1,10 +1,12 @@
 package edu.brown.cs32.goingrogue.gameobjects.items.factories;
 
 import edu.brown.cs32.goingrogue.gameobjects.creatures.Attribute;
+import edu.brown.cs32.goingrogue.gameobjects.creatures.Creature;
 import edu.brown.cs32.goingrogue.gameobjects.items.GridItem;
 import static edu.brown.cs32.goingrogue.gameobjects.creatures.util.RandomDataUtil.*;
 import edu.brown.cs32.goingrogue.gameobjects.items.ItemStats;
 import edu.brown.cs32.goingrogue.util.CreatureSize;
+import edu.brown.cs32.jcadler.GameLogic.RogueMap.Room;
 import java.awt.geom.Point2D;
 import java.util.List;
 
@@ -14,7 +16,7 @@ import java.util.List;
  */
 public class GridItemFactory {
 
-    public static GridItem create() {
+    public static GridItem create(List<Creature> creatures, List<Room> rooms) {
         List<Attribute> attributes = randomItemAttributes();
         String name = getItemName(attributes);
         String sprite = getSprite(attributes);
