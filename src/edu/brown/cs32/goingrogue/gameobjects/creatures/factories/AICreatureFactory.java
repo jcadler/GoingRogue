eb857ebaaa80a671e3525c4810de46cf546bb7c0
Ben Weedon <Benjamin_Weedon@brown.edu>
2013-05-08 04:13:59 -0400

    Scale monsters now.

--- src/edu/brown/cs32/goingrogue/gameobjects/creatures/factories/AICreatureFactory.java
+++ src/edu/brown/cs32/goingrogue/gameobjects/creatures/factories/AICreatureFactory.java
@@ -20,7 +20,7 @@ public class AICreatureFactory {
 
     public static AICreature create(List<Creature> creatures, List<Room> rooms, int playerLevel) {
         double direction = Math.PI / 2.0;
-        List<Attribute> attributes = randomCreatureAttributes();
+        List<Attribute> attributes = randomCreatureAttributes(playerLevel);
         String name = getCreatureName(attributes);
         String sprite = getSprite(attributes);
         CreatureStats stats = randomCreatureStats(attributes);
