00f948c17d31db17940f1e0b72d4f565690d55fc
Ben Weedon <Benjamin_Weedon@brown.edu>
2013-05-08 00:34:00 -0400

    New potions, and hopefully knockback works.

---
+++ src/edu/brown/cs32/goingrogue/gameobjects/items/AttackPotion.java
@@ -0,0 +1,19 @@
+package edu.brown.cs32.goingrogue.gameobjects.items;
+
+import edu.brown.cs32.goingrogue.gameobjects.creatures.Creature;
+
+/**
+ *
+ * @author Ben Weedon (bweedon)
+ */
+public class AttackPotion extends Potion {
+
+    public AttackPotion(GridItem gridItem) {
+        super(gridItem);
+    }
+
+    @Override
+    public void act(Creature creature) {
+        creature.getStats().setAttack(creature.getStats().getAttack() + 10.0);
+    }
+}
