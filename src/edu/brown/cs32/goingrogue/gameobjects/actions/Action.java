8d0331169dfe4e4c176bdaedb170b38803cb8d17
Ben Weedon <Benjamin_Weedon@brown.edu>
2013-05-09 21:34:16 -0400

    Action has toString

--- src/edu/brown/cs32/goingrogue/gameobjects/actions/Action.java
+++ src/edu/brown/cs32/goingrogue/gameobjects/actions/Action.java
@@ -4,25 +4,26 @@ import java.util.List;
 
 import edu.brown.cs32.goingrogue.gameobjects.creatures.Creature;
 
-/** 
+/**
  *
  * @author Ben Weedon (bweedon)
  */
 public abstract class Action {
 
-	protected ActionType _type;
-	
+    protected ActionType _type;
     private int _timer;
     private Range _range;
-    
-    public Action(){}
+
+    public Action() {
+    }
+
     public Action(int timer, Range range) {
-    	_timer = timer;
+        _timer = timer;
         _range = range;
     }
-    
+
     public ActionType type() {
-    	return _type;
+        return _type;
     }
 
     public Creature actOnClone(Creature creature) throws CloneNotSupportedException {
@@ -30,7 +31,7 @@ public abstract class Action {
         act(clone);
         return clone;
     }
-    
+
     public int getTimer() {
         return _timer;
     }
@@ -39,15 +40,21 @@ public abstract class Action {
         _timer -= delta;
         _range.decrementTimer(delta);
     }
-    
+
     public boolean withinRange(Creature creature) {
         return _range.inRange(creature);
     }
-    
+
     public Range getRange() {
         return _range;
     }
 
     public abstract void act(Creature creature);
+
     public abstract List<ActionAnimation> getActionAnimations();
+
+    @Override
+    public String toString() {
+        return "Action{" + "_type=" + _type + ", _timer=" + _timer + ", _range=" + _range + '}';
+    }
 }
